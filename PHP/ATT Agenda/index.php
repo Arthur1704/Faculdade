9fa024a08183862a189bcb6086141dfe95ca444e
Arthur Viana <arthurcruzviana19@gmail.com>
2025-03-13 16:55:32 -0300

    Com css

--- PHP/ATT Agenda/index.php
+++ PHP/ATT Agenda/index.php
@@ -5,9 +5,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Agenda</title>
+        <link rel="stylesheet" href="css.css">
     </head>
     <body>
         <div id="caixa_principal">
+            <h2>Agenda</h2>
             <form action="index.php" method="POST">
                 <label>Nome:</label><br>
                 <input type="text" name="nome" size="50px" required>
@@ -18,55 +20,54 @@
                 <label>Altura:</label><br>
                 <input type="text" name="altura" size="20px">
                 <br>
-                <br>
                 <label>Posição:</label><br>
                 <input type="number" name="posi" size="20px">
-                <br>
+                <br><br>
                 <input type="submit" class="botoes" name="guardar" value="Guardar">
                 <input type="submit" class="botoes" name="apagar" value="Apagar">
                 <input type="submit" class="botoes" name="posicao" value="Posicao">
                 <input type="submit" class="botoes" name="verAgenda" value="Ver Agenda">
                 <input type="submit" class="botoes" name="verPessoa" value="Ver Pessoa">
             </form>
+            
+        <?php
+            include_once 'ClassAgenda.php';
+            include_once 'ClassPessoa.php'; 
+
+            $pessoa = new Pessoa();
+            $agenda = new Agenda();
+
+        if(isset($_POST['guardar'])){
+
+                $nome = $_POST['nome'];
+                $idade = $_POST['idade'];
+                $altura = $_POST['altura'];
+
+                $pessoa->setPessoa($nome, $idade, $altura);
+
+            }
+            else if (isset($_POST['apagar'])){
+                $nome = $_POST['nome'];
+
+                $pessoa->Apagar($nome);
+            }
+            else if (isset($_POST['posicao'])){
+                $nome = $_POST['nome'];
+                $retorno = $agenda->Busca($nome);
+                if($retorno == 0){
+                    echo "<script>alert('$nome não foi achado na agenda!!')</script>";
+                }else{
+                    echo "<script>alert('$nome está na posição $retorno!!')</script>";
+                }
+            }
+            else if(isset($_POST['verAgenda'])){
+                $agenda->mostarAgenda();
+            }
+            else if(isset($_POST['verPessoa'])){
+                $posiP = $_POST['posi'];
+                $pessoa->verPessoa($posiP);
+            }
+        ?>
         </div>
     </body>
 </html>
-
-<?php
-    include_once 'ClassAgenda.php';
-    include_once 'ClassPessoa.php'; 
-
-    $pessoa = new Pessoa();
-    $agenda = new Agenda();
-
-   if(isset($_POST['guardar'])){
-
-        $nome = $_POST['nome'];
-        $idade = $_POST['idade'];
-        $altura = $_POST['altura'];
-
-        $pessoa->setPessoa($nome, $idade, $altura);
-
-    }
-    else if (isset($_POST['apagar'])){
-        $nome = $_POST['nome'];
-
-        $pessoa->Apagar($nome);
-    }
-    else if (isset($_POST['posicao'])){
-        $nome = $_POST['nome'];
-        $retorno = $agenda->Busca($nome);
-        if($retorno == 0){
-            echo "<script>alert('$nome não foi achado na agenda!!')</script>";
-        }else{
-            echo "<script>alert('$nome está na posição $retorno!!')</script>";
-        }
-    }
-    else if(isset($_POST['verAgenda'])){
-        $agenda->mostarAgenda();
-    }
-    else if(isset($_POST['verPessoa'])){
-        $posiP = $_POST['posi'];
-        $pessoa->verPessoa($posiP);
-    }
-?>
