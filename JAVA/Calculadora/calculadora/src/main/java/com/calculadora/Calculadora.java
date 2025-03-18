13798a82a357d025e1835efe323c27999c520057
Arthur <arthurcruzviana19@gmail.com>
2025-03-18 10:29:32 -0300

    Aprimorando Calculadora JAVA

--- JAVA/Calculadora/calculadora/src/main/java/com/calculadora/Calculadora.java
+++ JAVA/Calculadora/calculadora/src/main/java/com/calculadora/Calculadora.java
@@ -2,23 +2,31 @@ package com.calculadora;
 
 public class Calculadora{
 
-    public static double Calcular(double num1, double num2, int op){
+    public static double Soma(double num1, double num2){
 
-        switch (op) {
-            case 1:
-                return  num1 + num2;
+        return num1 + num2;
 
-            case 2:
-                return num1 - num2;
+    }
 
-            case 3:
-                return num1 * num2;
+    public static double Subtracao(double num1, double num2){
 
-            case 4:
-                return num1 / num2;
-            
-            default:
-                return 0;    
+        return num1 - num2;
+
+    }
+
+    public static double Multiplicacao(double num1, double num2){
+
+        return num1 * num2;
+
+    }
+
+    public static double Divisao(double num1, double num2){
+        if(num2 != 0){
+            return num1 / num2;
         }
+        else{
+            return 0;
+        }
+
     }
 }
--- JAVA/Calculadora/calculadora/src/main/java/com/calculadora/Main.java
+++ JAVA/Calculadora/calculadora/src/main/java/com/calculadora/Main.java
@@ -13,30 +13,58 @@ public class Main {
     }
     public static void main(String[] args) {
 
-        double num1, num2;
+        double num1, num2, resultado = 0;
         int operacao;
+        char S_N = 'N';
+
+        do{
 
         System.out.println("1 - SOMA");
         System.out.println("2 - SUBTRAÇÂO");
         System.out.println("3 - MULTIPLICAÇÃO");
-        System.out.println("4 - MULTIPLICAÇÃO");
+        System.out.println("4 - DIVISÃO");
         System.out.println("5 - CALCULAR IMPOSTO");
 
         System.out.print("\n Entre com a opção desejada: ");
         operacao = guardar.nextInt();
-
-        num1 = set_num();
+        
+        if (S_N == 'N'){
+            num1 = set_num();
+        }
+        else{
+            num1 = resultado;
+        }
         num2 = set_num();
 
-        switch (operacao) {
-            case 1:
-                
-                break;
         
-            default:
-                break;
-        }
-        System.out.println(Calculadora.Calcular(num1, num2, operacao));
+            switch (operacao) {
+                case 1:
+                    resultado = Calculadora.Soma(num1, num2);
+                    System.out.println("O resultado da sua soma foi " + Calculadora.Soma(num1, num2));
+                    break;
+                case 2:
+                    resultado = Calculadora.Subtracao(num1, num2);
+                    System.out.println("O resultado da sua subtração foi " + Calculadora.Subtracao(num1, num2));   
+                    break;
+                case 3:
+                    resultado = Calculadora.Multiplicacao(num1, num2);
+                    System.out.println("O resultado da sua multiplicação foi " + Calculadora.Multiplicacao(num1, num2)); 
+                    break;
+                case 4:
+                    resultado = Calculadora.Divisao(num1, num2);
+                    System.out.println("O resultado da sua divisão foi " + Calculadora.Divisao(num1, num2));
+                    break;
+                case 5:
+                    //System.out.println("O resultado da sua soma foi " + Calculadora.Calcular(num1, num2));    
+                    break;
+                default:
+                    System.out.println("Entre com uma opção correta!!");
+                    break;
+            }
+            System.out.print("Deseja fazer uma operação com este resultado (S ou N) ?");
+            S_N = guardar.next().charAt(0);
+            S_N = Character.toUpperCase(S_N);
+        }while(S_N == 'S');
 
         guardar.close();
 
