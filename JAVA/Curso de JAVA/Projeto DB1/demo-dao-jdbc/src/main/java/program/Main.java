0cb112b8232dd0b525977b9e2cabf074e1a98f09
Arthur <arthurcruzviana19@gmail.com>
2025-07-17 13:39:04 -0300

    Insert

--- JAVA/Curso de JAVA/Projeto DB1/demo-dao-jdbc/src/main/java/program/Main.java
+++ JAVA/Curso de JAVA/Projeto DB1/demo-dao-jdbc/src/main/java/program/Main.java
@@ -5,6 +5,7 @@ import dao.SellerDao;
 import entities.Department;
 import entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -29,5 +30,11 @@ public class Main {
         for (Seller seller2 : listAll){
             System.out.println(seller2);
         }
+
+        System.out.println("\n=== TEST 4: Insert ===");
+        Seller newSeller = new Seller(0, "Arthur Viana",new Date(), "arthur@gmail.com", 1000.00, dep);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserido, ID do vendedor: " + newSeller.getId());
+
     }
 }
