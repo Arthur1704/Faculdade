450877f351f22483b69d9418954cb602c3ba01cd
Arthur Viana <arthurcruzviana19@gmail.com>
2025-07-18 21:27:00 -0300

    Department

--- JAVA/Curso de JAVA/Projeto DB1/demo-dao-jdbc/src/main/java/dao/impl/DepartmentDaoJDBC.java
+++ JAVA/Curso de JAVA/Projeto DB1/demo-dao-jdbc/src/main/java/dao/impl/DepartmentDaoJDBC.java
@@ -6,6 +6,7 @@ import utilities.conectionDB.DB;
 import utilities.conectionDB.DBExeption;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
@@ -72,16 +73,71 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void deleteById(Integer id) {
-        
+        PreparedStatement ps = null;
+        try{
+            ps = conn.prepareStatement("DELETE FROM department WHERE id = ?");
+            ps.setInt(1,id);
+
+            ps.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new DBExeption(e.getMessage());
+        }
+        finally {
+            DB.closeStatment(ps);
+        }
     }
 
     @Override
     public Department findById(Integer id) {
-        return null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try{
+            ps = conn.prepareStatement(
+                    "SELECT department.* FROM department "
+                    + "WHERE id = ?");
+
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+
+            if (rs.next()){
+                return instantiateDepartiment(rs);
+            }
+            return null;
+        }
+        catch (SQLException e){
+            throw new DBExeption(e.getMessage());
+        }
+        finally {
+            DB.closeStatment(ps);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
     public List<Department> findAll() {
-        return List.of();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Department> list = new ArrayList<>();
+        try{
+            ps = conn.prepareStatement("SELECT department.* FROM department");
+            rs = ps.executeQuery();
+
+            while(rs.next()){
+                list.add(instantiateDepartiment(rs));
+            }
+            return list;
+        }
+        catch (SQLException e){
+            throw new DBExeption(e.getMessage());
+        }
+    }
+
+    private Department instantiateDepartiment(ResultSet rs) throws SQLException{
+        Department dep = new Department();
+        dep.setId(rs.getInt("Id"));
+        dep.setName(rs.getString("Name"));
+        return dep;
     }
 }
