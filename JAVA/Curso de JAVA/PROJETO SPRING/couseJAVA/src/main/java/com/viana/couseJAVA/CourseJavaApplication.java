fd274ffac5ef08e625817e145898734deaa85638
Arthur1704 <arthurcruzviana19@gmail.com>
2025-07-21 00:58:11 -0300

    DB H2 conectando

--- JAVA/Curso de JAVA/PROJETO SPRING/couseJAVA/src/main/java/com/viana/couseJAVA/CourseJavaApplication.java
+++ JAVA/Curso de JAVA/PROJETO SPRING/couseJAVA/src/main/java/com/viana/couseJAVA/CourseJavaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CouseJavaApplication {
+public class CourseJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CouseJavaApplication.class, args);
+		SpringApplication.run(CourseJavaApplication.class, args);
 	}
 
 }
