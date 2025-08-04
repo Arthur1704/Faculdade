ea2b280a9723a189ed84c0e671a0d0eef16cd9a6
Arthur1704 <arthurcruzviana19@gmail.com>
2025-08-04 19:06:19 -0300

    Place piece in the board

--- Projetos Arthur/JAVA/Chess-System/chess/src/main/java/boardgame/Board.java
+++ Projetos Arthur/JAVA/Chess-System/chess/src/main/java/boardgame/Board.java
@@ -28,11 +28,16 @@ public class Board {
         this.columns = columns;
     }
 
-    public Piece piece(int row, int colunm){
+    public Piece piece(int row, int colunm) {
         return pieces[row][colunm];
     }
     
     public Piece piece (Position position){
         return pieces[position.getRow()][position.getColunm()];
+    }
+
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColunm()] = piece;
+        piece.position = position;
     }
 }
