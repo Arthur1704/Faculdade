e50374b5cc36cfd2712c897faceee0f3ddee3d65
Arthur <arthurcruzviana19@gmail.com>
2025-08-08 15:19:36 -0300

    Moving pieces

--- Projetos Arthur/JAVA/Chess-System/chess/src/main/java/boardgame/Board.java
+++ Projetos Arthur/JAVA/Chess-System/chess/src/main/java/boardgame/Board.java
@@ -45,6 +45,19 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not board");
+        }
+        if (piece(position) == null){
+            return  null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColunm()] = null;
+        return aux;
+    }
+
     private boolean positionExists(int row, int collum){
         return row >= 0 && row < rows && collum >= 0 && collum < columns;
     }
