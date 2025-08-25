ae323c927df6b3366c0e9d586bb70d45943b77bf
Arthur <arthurcruzviana19@gmail.com>
2025-08-25 10:23:45 -0300

    Checkmate logic

--- Projetos Arthur/JAVA/Chess-System/chess/src/main/java/application/Program.java
+++ Projetos Arthur/JAVA/Chess-System/chess/src/main/java/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while(true) {
+        while(!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch, captured);
@@ -43,10 +43,9 @@ public class Program {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
-            catch (InputMismatchException e){
-                System.out.println(e.getMessage());
-                sc.nextLine();
-            }
+            sc.nextLine();
         }
     }
+    UI.clearScreen();
+    UI.printMatch(chessMatch, captured);
 }
