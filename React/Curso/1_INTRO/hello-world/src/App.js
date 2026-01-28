4eab2c99653e8a09b9b1e391b36870a73bc94cbb
Arthur <arthurcruzviana19@gmail.com>
2026-01-28 08:48:32 -0300

    Primeira função em React

--- React/Curso/1_INTRO/hello-world/src/App.js
+++ React/Curso/1_INTRO/hello-world/src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Login() {
+    return (
+    <div className="tarefa">
+      <form action="">
+        <input type="text" name="login" id="login" />
+        <input type="text" name="senha" id="senha" />
+      </form>
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
         <h1>Hello Word</h1>
+        <Login />
     </div>
-  );
+  )
 }
+
+
+
 
 export default App;
