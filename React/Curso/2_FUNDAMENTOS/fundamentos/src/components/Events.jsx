e517fb278118a69049cc0cab443fca3a248ac92a
Arthur1704 <arthurcruzviana19@gmail.com>
2026-02-05 23:17:42 -0300

    In line

--- React/Curso/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ React/Curso/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -10,6 +10,9 @@ const Events = () => {
             <div>
                 <button onClick={handleMyEvent}>Clique aqui!!</button>
             </div>
+            <div>
+                <button onClick={() => alert("Function in line")}>Clique aqui tambem!!</button>
+            </div>
         </div>
     );
 };
