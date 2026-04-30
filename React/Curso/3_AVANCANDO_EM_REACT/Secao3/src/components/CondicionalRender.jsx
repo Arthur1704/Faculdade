51ca89b2b37db942c9a88bcdfd8dcdbe3bce1b9c
Arthur <arthurcruzviana19@gmail.com>
2026-04-30 14:31:56 -0300

    If ternario

--- React/Curso/3_AVANCANDO_EM_REACT/Secao3/src/components/CondicionalRender.jsx
+++ React/Curso/3_AVANCANDO_EM_REACT/Secao3/src/components/CondicionalRender.jsx
@@ -2,11 +2,24 @@ import { useState } from "react";
 
 const CondicionalRender = () => {
     const [x] = useState(false);
+    const [name, setName] = useState("Maria");
   return (
     <div>
         <h1>Isso sera exibido ?</h1>
         {x && <p>Se x for true, sim</p>}
         {!x && <p>Agora x é falso</p>}
+
+        {name === "Arthur" ? (
+            <div>
+                <p>O nome é Arthur!!</p>
+            </div>
+        ) : (
+            <div>
+                <p>Nome não encontrado!!</p>
+            </div>
+        )}
+
+        <button onClick={() => setName("Arthur")}>Clique aqui!!</button>
     </div>
   )
 }
