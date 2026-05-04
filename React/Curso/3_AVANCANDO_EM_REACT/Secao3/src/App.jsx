3d9b405a092b408df182ef471dafb1d551e1ba94
Arthur <arthurcruzviana19@gmail.com>
2026-05-04 10:04:22 -0300

    Props

--- React/Curso/3_AVANCANDO_EM_REACT/Secao3/src/App.jsx
+++ React/Curso/3_AVANCANDO_EM_REACT/Secao3/src/App.jsx
@@ -4,9 +4,11 @@ import Floresta from "./assets/img2.jpg";
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import CondicionalRender from './components/CondicionalRender';
+import ShowUserName from './components/ShowUserName';
 
 function App() {
   const [count, setCount] = useState(0)
+  const name = "Maria";
 
   return (
     <div className="App">
@@ -22,6 +24,7 @@ function App() {
       <ManageData />
       <ListRender />
       <CondicionalRender />
+      <ShowUserName name={name}/>
     </div>
 
   )
