dc6a3f1e907cdd50488e989c6d8303260509815e
Arthur <arthurcruzviana19@gmail.com>
2026-05-06 09:59:12 -0300

    CSS MODULES

--- React/Curso/4_CSS_REACT/css/src/App.jsx
+++ React/Curso/4_CSS_REACT/css/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import MyComponent from './components/MyComponent'
+import Title from './components/Title';
 
 function App() {
   const [count, setCount] = useState(0)
   const num = 12;
+  const title = false
 
   return (
     <div>
@@ -16,6 +18,10 @@ function App() {
       <p style={{color: "white"}}>Este elemento foi estilizado de forma inline</p>
       {/* CSS INLINE DINAMICO */}
       <h2 style={num < 10 ? ({color: "blue"}) : ({color: "Magenta"})}>CSS Dinamico</h2>
+      {/* CLASSE DINAMICA */}
+      <h2 className={title ? "title" : "title2"}>Esse elemento tem uma classe dinamica</h2>
+      {/* CSS Modules */}
+      <Title />
     </div>
   )
 }
