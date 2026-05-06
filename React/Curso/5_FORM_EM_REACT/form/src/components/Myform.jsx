da717abd72f1011604397fc08a65736f2d951669
Arthur <arthurcruzviana19@gmail.com>
2026-05-06 13:20:44 -0300

    Pegar o input atravez do onChange

--- React/Curso/5_FORM_EM_REACT/form/src/components/Myform.jsx
+++ React/Curso/5_FORM_EM_REACT/form/src/components/Myform.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react"
 import "./Myform.css"
 
 const Myform = () => {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+       setName(e.target.value);
+    }
+
+    console.log(name);
+
   return (
     <div>
         {/* 1 - Crianção do form */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite seu nome"/>
+                <input type="text" name="name" placeholder="Digite seu nome" onChange={handleName}/>
             </div>
             {/* 2 - Label envolvendo Input */}
             <label>
