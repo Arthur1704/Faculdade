4ea92769b1494002f233441eb0aba1831c3ddbab
Arthur <arthurcruzviana19@gmail.com>
2026-05-06 13:12:42 -0300

    Lalbel envolvendo input

--- React/Curso/5_FORM_EM_REACT/form/src/components/Myform.jsx
+++ React/Curso/5_FORM_EM_REACT/form/src/components/Myform.jsx
@@ -9,6 +9,11 @@ const Myform = () => {
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" placeholder="Digite seu nome"/>
             </div>
+            {/* 2 - Label envolvendo Input */}
+            <label>
+                <span>E-mail: </span>
+                <input type="email" name="email" placeholder="Digite seu email"/>
+            </label>
             <input type="submit" value="Enviar" />
         </form>
     </div>
