0cbe2de29f0ae050aee6a3f2dfbfda8fbf353180
Arthur <arthurcruzviana19@gmail.com>
2026-05-14 14:31:16 -0300

    Buscando dados da API

--- React/Curso/7_REQ_HTTP/httpreact/src/App.jsx
+++ React/Curso/7_REQ_HTTP/httpreact/src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
 
+import { useState, useEffect } from "react"
+
+const url = "http://localhost:3000/products";
 function App() {
-  
+  const [products, setProducts] = useState([]);
+
+  // 1 - Resgatando Dados
+  // Atualizado
+  useEffect(() => {
+    async function fechdata() {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fechdata();
+  }, []);
 
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
