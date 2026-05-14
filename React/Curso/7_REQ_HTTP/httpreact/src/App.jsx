8096391f88c4c197f65fae1747a38bc9e37f0279
Arthur <arthurcruzviana19@gmail.com>
2026-05-14 15:51:04 -0300

    POST

--- React/Curso/7_REQ_HTTP/httpreact/src/App.jsx
+++ React/Curso/7_REQ_HTTP/httpreact/src/App.jsx
@@ -6,8 +6,10 @@ const url = "http://localhost:3000/products";
 function App() {
   const [products, setProducts] = useState([]);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   // 1 - Resgatando Dados
-  // Atualizado
   useEffect(() => {
     async function fechdata() {
       const res = await fetch(url);
@@ -18,6 +20,13 @@ function App() {
     fechdata();
   }, []);
 
+  // 2 - Adição de Produtos
+
+  const handleSubmit = async (e) => {
+    
+  }
+
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
@@ -26,6 +35,19 @@ function App() {
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome: 
+            <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}/>
+          </label>
+          <label>
+            Preço: 
+            <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
+          </label>
+          <input type="submit" value={Criar} />
+        </form>
+      </div>
     </div>
   )
 }
