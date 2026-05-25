2b433c6f780650aca4eb7aee87bef282c6307e4c
Arthur1704 <arthurcruzviana19@gmail.com>
2026-05-24 22:02:26 -0300

    Link e Pages

--- React/Curso/8_REACT_ROUTER/reactrouter/src/App.jsx
+++ React/Curso/8_REACT_ROUTER/reactrouter/src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css'
 
+// 1 - Config react Router
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+// Pages
+import Home from './pages/Home'
+import About from './pages/About'
+
+//Components
+import NavBar from './components/NavBar'
+
 function App() {
 
 
   return (
     <div className='App'>
+      <h1>React Router</h1>
+      <BrowserRouter>
+       <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
