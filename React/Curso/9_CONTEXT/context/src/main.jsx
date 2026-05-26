174e7685a649fac9184e92133c579cc0566023db
Arthur <arthurcruzviana19@gmail.com>
2026-05-26 12:48:01 -0300

    Context mais complexo

--- React/Curso/9_CONTEXT/context/src/main.jsx
+++ React/Curso/9_CONTEXT/context/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 
 import { ConstContextProvider } from './context/CounterContext.jsx'
+import { TitleColorContextProvider } from './context/TitleColorContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* 2 - Criando Provider */}
     <ConstContextProvider>
-      <App />
+      <TitleColorContextProvider>
+        <App />
+      </TitleColorContextProvider>
     </ConstContextProvider>
   </StrictMode>,
 )
