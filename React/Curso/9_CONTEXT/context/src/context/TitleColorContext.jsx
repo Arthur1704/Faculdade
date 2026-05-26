1a4eea8e9234d8c8d9c3019b4e4cb300d885d47c
Arthur <arthurcruzviana19@gmail.com>
2026-05-26 13:07:36 -0300

    Alterando valor de um context complexo

--- React/Curso/9_CONTEXT/context/src/context/TitleColorContext.jsx
+++ React/Curso/9_CONTEXT/context/src/context/TitleColorContext.jsx
@@ -4,8 +4,15 @@ import { createContext, useReducer } from "react";
 export const TitleColorContext = createContext()
 
 export const titleColorReducer  = (state, action) => {
-    
-}
+    switch (action.type) {
+        case "RED":
+            return {...state, color: "red"}
+        case "BLUE":
+            return {...state, color: "blue"}
+        default:
+            return state;
+    }
+};
 
 export const TitleColorContextProvider = ({children}) => {
 
@@ -13,6 +20,6 @@ export const TitleColorContextProvider = ({children}) => {
 
    console.log("Title color context: ", state)
    return(
-        <TitleColorContext.Provider value={{...state}}>{children}</TitleColorContext.Provider>
+        <TitleColorContext.Provider value={{...state, dispatch}}>{children}</TitleColorContext.Provider>
    );
 };
--- React/Curso/9_CONTEXT/context/src/pages/About.jsx
+++ React/Curso/9_CONTEXT/context/src/pages/About.jsx
@@ -1,11 +1,13 @@
 import {useContext} from 'react'
 import { CounterContext } from "../context/CounterContext";
+import { useTitleColorContext } from '../hooks/useTitleColorContext';
 const About = () => {
 
   const {counter} = useContext(CounterContext);
+  const {color} = useTitleColorContext();
   return (
     <div>
-      <h1>About</h1>
+      <h1 style={{color: color}}>About</h1>
       <p>Valor do contador: {counter}</p>
     </div>
   )
