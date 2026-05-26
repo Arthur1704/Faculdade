b42021686e780eae1de1a75f27b01a7f957b3a79
Arthur <arthurcruzviana19@gmail.com>
2026-05-26 10:16:58 -0300

    Hook Context

--- React/Curso/9_CONTEXT/context/src/pages/Home.jsx
+++ React/Curso/9_CONTEXT/context/src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import { useContext } from 'react'
-import {CounterContext} from '../context/CounterContext'
 import ChangeCounter from '../components/ChangeCounter';
+import { useCounterContext } from '../hooks/useCounterContext';
 
 const Home = () => {
 
-  const {counter} = useContext(CounterContext);
-
+  // const {counter} = useContext(CounterContext);
+  const {counter} = useCounterContext();
   return (
     <div>
       <h1>Home</h1>
