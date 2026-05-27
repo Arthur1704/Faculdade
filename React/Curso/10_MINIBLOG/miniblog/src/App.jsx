a621da65ce9908ab739e75f53dc7ff518756ae8a
Arthur <arthurcruzviana19@gmail.com>
2026-05-27 08:29:00 -0300

    Estilos navBar e Footer

--- React/Curso/10_MINIBLOG/miniblog/src/App.jsx
+++ React/Curso/10_MINIBLOG/miniblog/src/App.jsx
@@ -1,11 +1,25 @@
 import "./App.css"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// PAGES
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 function App() {
-
 
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
