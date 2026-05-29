2564f344a50daedb06d1ca8f041fa526bcfdba61
Arthur <arthurcruzviana19@gmail.com>
2026-05-29 08:30:30 -0300

    Cadastrando usuario

--- React/Curso/10_MINIBLOG/miniblog/src/firebase/config.jsx
+++ React/Curso/10_MINIBLOG/miniblog/src/firebase/config.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAE1M5glfVDZIrNd3OZOhJFqvkbC7Sb2xc",
