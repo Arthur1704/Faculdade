18e9362906275d6441ea08e6d2b37e030d455a28
Arthur1704 <arthurcruzviana19@gmail.com>
2026-05-31 01:01:17 -0300

    Post

---
+++ React/Curso/10_MINIBLOG/miniblog/src/hooks/useInsertDocument.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useReducer, useState } from "react"
+import {db} from "../firebase/config"
+import { addDoc, collection, Timestamp } from "firebase/firestore"
+
+
+const initialState = {
+    loading: null,
+    error: null
+}
+
+const insertReducer = (state, action) =>{
+    switch(action.type){
+        case "LOADING":
+            return {loading: true, error:null};
+        case "INSERTED_DOC":
+            return {loading: false, error:null};
+        case "ERROR":
+            return {loading:false, error:action.payLoad};
+        default:
+            return state;
+    }
+}
+
+export const useInsertDocument = (docCollection) =>{
+   
+
+    const [response, dispatch] = useReducer(insertReducer, initialState)
+
+    const [cancelled, setCancelled] = useState(false)
+
+    const checkCancelBeforeDispatch = (action) => {
+        if (!cancelled){
+            dispatch(action)
+        }
+    }
+
+    const insertDocument = async(document) => {
+        checkCancelBeforeDispatch({
+            type: "LOADING",
+        });
+        try{
+            const newDocument = {...document, createAt: Timestamp.now()}
+
+            const insertedDocument = await addDoc(
+                collection(db, docCollection),
+                newDocument
+            )
+
+            checkCancelBeforeDispatch({
+                type: "INSERTED_DOC",
+                payLoad: insertedDocument
+            })
+        }
+        catch(error){
+            console.error("Erro Firebase:", error);
+            checkCancelBeforeDispatch({
+                type: "ERROR",
+                payLoad: error.message
+            })
+        }
+    }
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, )
+
+    return {insertDocument, response};
+
+
+}
--- React/Curso/10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
+++ React/Curso/10_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -1,9 +1,88 @@
+import { useState } from "react";
 import styles from "./CreatePost.module.css"
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+import {useAuthValue} from "../../context/AuthContext"
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const {user} = useAuthValue();
+
+  const {insertDocument, response} = useInsertDocument("posts")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setFormError("")
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName
+    })
+  };
+
+
+
   return (
-    <div>
-        <h1>CreatePost</h1>
+    <div className={styles.create_post}>
+        <h2>Criar Post</h2>
+        <p>Escreva sobre o que quiser e compartilheo seu conhecimento!!</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Titulo:</span>
+            <input 
+              type="text" 
+              name="title"
+              placeholder="Pense em um bom titulo..."
+              required
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
+          </label>
+          <label>
+            <span>URL da imagem:</span>
+            <input 
+              type="text" 
+              name="image"
+              placeholder="Insira uma imagem que represente o seu post!"
+              required
+              onChange={(e) => setImage(e.target.value)}
+              value={image}
+            />
+          </label>
+          <label>
+            <span>Conteudo:</span>
+            <textarea 
+            name="body" 
+            placeholder="Insira o conteudo do post" 
+            required
+            onChange={(e) => setBody(e.target.value)}
+            value={body}>
+            </textarea>
+          </label>
+          <label>
+            <span>Tags:</span>
+            <input 
+              type="text" 
+              name="tags"
+              placeholder="Insira as tags separadas por virgula!"
+              required
+              onChange={(e) => setTags(e.target.value)}
+              value={tags}
+            />
+          </label>        
+          {!response.loading &&  <button className='btn'>Cadastrar</button>}
+          {response.loading &&  <button className='btn' disabled>Aguarde...</button>}        
+          {response.error && <p className='error'>{response.error}</p>}
+        </form>
     </div>
   )
 }
