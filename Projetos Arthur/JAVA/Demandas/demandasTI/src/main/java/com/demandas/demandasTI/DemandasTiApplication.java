d00e731de1dbcab99f7ef3ac144f540511a9784c
Arthur1704 <arthurcruzviana19@gmail.com>
2026-06-13 13:07:37 -0300

    API Demandas

---
+++ Projetos Arthur/JAVA/Demandas/demandasTI/src/main/java/com/demandas/demandasTI/DemandasTiApplication.java
@@ -0,0 +1,13 @@
+package com.demandas.demandasTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemandasTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemandasTiApplication.class, args);
+	}
+
+}
